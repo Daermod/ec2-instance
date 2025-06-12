c729c4d9b1434f4380807e38a1d35ec539e8dffa
Ryan <ryan.m.ashbridge@gmail.com>
2025-06-12 14:15:10 -0400

    restructured the file

--- userdata.sh
+++ userdata.sh
@@ -6,9 +6,9 @@ sudo apt update
 # Install NGINX
 sudo apt install nginx
 
+# Start and enable NGINX
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
 # Setting a custom Index.HTML page
 echo "<h3> Welcome to my EC2 Instance for the EC2 Instance DevOps Project on roadmap.sh!<h3>" > /var/www/html/index.html
-
-# Start and enable NGINX
-sudo systemctl start nginx
-sudo systemctl enable nginx
