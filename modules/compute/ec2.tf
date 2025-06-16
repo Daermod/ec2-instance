4a1695b9a92a684231dd00caefba886bfd201cb1
Ryan <ryan.m.ashbridge@gmail.com>
2025-06-16 13:36:40 -0400

    done

--- modules/compute/ec2.tf
+++ modules/compute/ec2.tf
@@ -1,24 +1,32 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250610"]
+    name    = "name"
+    values  = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20250610"]
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("././userdata.tpl")
+
+  vars = {
+    html_content  = file("././index.html")
   }
 }
 
 resource "aws_instance" "web_server" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
-  vpc_security_group_ids = [
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = var.instance_type
+  subnet_id               = var.subnet_id
+  vpc_security_group_ids  = [
       var.sg_allow_http,
       var.sg_allow_ssh
   ]
   key_name = var.key_pair_name
 
-  user_data = file("././userdata.sh")
+  user_data = data.template_file.user_data.rendered
 
   tags = {
     Name = "Web Server"
